e017fe3360322a4bdec97eaaed6b850e711ebdc6
Surya Chandra Rao Gandreddi <surya.iiit@gmail.com>
2020-05-10 09:48:28 +0000

    from repl

--- src/sample.c
+++ src/sample.c
@@ -4,7 +4,7 @@ Sankhya aarambham() {
 	Sankhya sum = SUNNA;
 	Vakhya statement = "The total is ";
 	sum = add(20, 22);
-	// dharshan(statement, sum);
+	dharshan(statement, sum);
 
 	tesuko SUNNA;
 }
